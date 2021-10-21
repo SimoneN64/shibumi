ed7ddab47f6a5306c019d866ee31a0493e7eaf44
simuuz <simococo03@gmail.com>
2021-10-21 20:14:19 +0200

    move utils/bit.h:raccess_X and waccess_X to utils/access.h and access.c respectively

--- include/utils/bit.h
+++ include/utils/bit.h
@@ -1,14 +1,4 @@
 #pragma once
 #include <stdlib.h>
-#include <common.h>
 
 size_t next_pow_2(size_t num);
-u16 raccess_16(u8* data, u32 index);
-u32 raccess_32(u8* data, u32 index);
-u64 raccess_64(u8* data, u32 index);
-void waccess_16(u8* data, u32 index, u16 val);
-void waccess_32(u8* data, u32 index, u32 val);
-void waccess_64(u8* data, u32 index, u64 val);
-
-#define raccess(size, data, index) raccess_##size(data, index)
-#define waccess(size, data, index, val) waccess_##size(data, index, val)
